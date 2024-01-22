af8f7f725074a58bf0bb571d84a5e7bffd46b4a0
Aslan <aslan.thr@gmail.com>
2024-01-21 21:21:40 -0500

    trying to solve

--- js/speller.js
+++ js/speller.js
@@ -1,30 +1,40 @@
 export default {
-	check,
-	lookup,
+  check,
+  lookup,
 };
 
 var elements;
 
 await loadPeriodicTable();
 
-
 // ****************************
 
 async function loadPeriodicTable() {
-	elements = await (await fetch("periodic-table.json")).json();
+  elements = await (await fetch("periodic-table.json")).json();
 }
 
 function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+  // TODO: determine if `inputWord` can be spelled
+  // with periodic table symbols; return array with
+  // them if so (empty array otherwise)
+  console.log("checking");
+  if (inputWord.length === 0) return [];
 
-	return [];
+  for (element char of inputWord) {
+	
+  }
+  return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
+  // TODO: return the element entry based on specified
+  // symbol (case-insensitive)
 
-	return {};
+  for (let element of elements) {
+    if (element.symbol.toLowerCase() === elementSymbol) {
+      return element;
+    }
+  }
+
+  return {};
 }
